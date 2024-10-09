7ae299aa5b43cfbb7965ce2b6a5bd6786dbe44f1
TheHardy04 <theo.hardy4@outlook.fr>
2024-10-09 16:43:56 +0900

    Stop tracking config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Constants for API and Telegram bot
-API_KEY = "Your API Key"
-TELEGRAM_TOKEN = "Your Telegram Token"
-CHAT_ID = "Your Chat ID"  # to find ChatID https://api.telegram.org/bot<YourBOTToken>/getUpdates
-TIME_INTERVAL = 120  # Time interval for sending updates (in seconds)
