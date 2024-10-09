7b8d99d0899a836c3bf706fc079e24d605076877
TheHardy04 <theo.hardy4@outlook.fr>
2024-10-09 16:06:52 +0900

    ChatBot Secure

---
+++ config.py
@@ -0,0 +1,6 @@
+
+# Constants for API and Telegram bot
+API_KEY = "JR9PWKZBEUESZW11T6GZXW8YC44638RUFW"
+TELEGRAM_TOKEN = "7678891919:AAEqJhce4dV3JWMPzUwOYcS6z64aXqNcKRc"
+CHAT_ID = "7577785128"  # to find ChatID https://api.telegram.org/bot<YourBOTToken>/getUpdates
+TIME_INTERVAL = 120  # Time interval for sending updates (in seconds)
